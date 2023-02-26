44fdeae6a6c2c0124fc9a61e3f4935eea5ac3532
Godbless Biekro <biekrogodbless@gmail.com>
2023-02-26 20:57:30 +0000

    string update

--- charr.c
+++ charr.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
 int main(void){
-    char name [20] = " Fafa Biekro";
+    char name[30];
 
-    printf("%s", &name[3]);
-    return 0;
+    printf("Please enter your name: ");
+    // scanf("%s %s", fname, lname);
+    gets(name);
+
+    printf("%.8s\n", name);
+    puts(name);
+
+        return 0;
 }
